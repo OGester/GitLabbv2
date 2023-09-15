e446024ef3a92d0625ddd282308119d8622fd815
Martin Phu <extramart@outlook.com>
2023-09-15 13:18:27 +0200

    second try

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Zucchinibiffar med yogurt\n");
         System.out.println("Ingredienser\n");
+        System.out.println("800g zucchini\n");
     }
 }
